77fbe395fdf453772558a27a34f3084805e077ed
lostya <lostyawolfer@gmail.com>
2026-05-11 09:50:13 +0200

    fix

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ from modules.bot_init import bot
 
 ################################################################
 
-version = 'v5.0.6-11'
+version = 'v5.0.6-12'
 
 changelog = \
 f"""
 ## {version} changelog
-- again & again & again
-  - like that one deleted modifier?
+- :fire:
 """
 
 ################################################################
@@ -1728,7 +1727,14 @@ async def create_challenge(
 @bot.event
 async def on_audit_log_entry_create(entry: discord.AuditLogEntry):
     if entry.action.value == 192:
-        new_status = getattr(entry.changes.after, "status", None)
+        raw_changes = getattr(entry, "_changes", [])
+        new_status = None
+        #old_status = None
+        for change in raw_changes:
+            if change.get("key") == "status":
+                new_status = change.get("new_value")
+                #old_status = change.get("old_value")
+                break
         if entry._target_id == config.channels['vc']:
             await general.send(config.message('edit_vc', member=entry.user.mention, status=new_status), pings=discord.AllowedMentions.none())
         elif entry._target_id == config.channels['vc2']:
